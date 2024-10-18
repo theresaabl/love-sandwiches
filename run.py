410d0d03594e8f44d0a4b14223b83436881cbd4f
Theresa Abl <theresaabl@gmx.at>
2024-10-18 09:33:23 +0000

    Resolve merge conflict, add validation to check for exactly 6 values in provided data

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 # mock terminal of 80 characters wide and 24 rows high
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -8,20 +9,39 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file("creds.json")
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("love_sandwiches")
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
     """
     print("Please enter sales data from the last market.")
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises VAlueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values"
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
 
 get_sales_data()
+
